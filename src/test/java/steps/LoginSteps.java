e6bd4643f5e6543b024d6e1b06cad773d7462cf8
svasenkov <aanher@gmail.com>
2021-09-18 13:02:04 +0300

    init project

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,31 @@
+package steps;
+
+public class LoginSteps {
+
+    //System.out.println("Step 1 Create new driver");
+
+    //здесь уже непосредственно шаги кукумбера идут
+
+    @Given("^Open login page")
+    public void openHomePage(){
+
+        System.out.println("first steps");
+    }
+
+//    @When("^Search in search field")
+//    public void searchData(){
+//        autoTests.searchOnPage();
+//    }
+//
+//   /* @When("^Get result")
+//    public void getResultSearch(){
+//        autoTests.getResult();
+//    }*/
+//
+//    @Then("^Get result")
+//    public void gettest11(){
+//        autoTests.main_window();
+//    }
+
+
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,6 @@
+Feature: Guess the word
+
+  Scenario: Breaker guesses a word
+  Given Open login page
+  When the Breaker makes a guess
+  Then the Maker is asked to score
